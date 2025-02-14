7516f15690e2be3534a0a272f75f33dcf7412c51
lcjao <111736803+lcjao@users.noreply.github.com>
2025-02-14 16:00:48 +0800

    Create CampusBookShareApplication.java
    主应用程序入口，包含 main 方法。

---
+++ CampusBookShareApplication.java
@@ -0,0 +1,14 @@
+package com.example.campusbookshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusBookShareApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CampusBookShareApplication.class, args);
+	}
+
+}
